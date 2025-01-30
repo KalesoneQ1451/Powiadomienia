38dcd3334c7a636fe776a1891bef1025d3cc93cc
KalesoneQ1451 <kacper.jatczak2006@gmail.com>
2025-01-30 11:35:21 +0100

    rampa

--- app/src/main/java/com/example/powiadomienia/MainActivity.java
+++ app/src/main/java/com/example/powiadomienia/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.powiadomienia;
 
+import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
@@ -25,7 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
         createNotificationChannel();
         Button notify = findViewById(R.id.notification);
+        Button notifyLong = findViewById(R.id.notificationLong);
         notify.setOnClickListener(v->sendNotification());
+        notifyLong.setOnClickListener(v->sendNotificationLong());
     }
 
     private void createNotificationChannel() {
@@ -41,10 +45,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendNotification(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
+                !=PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 1);
+                return;
+            }
+        }
         Intent intent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setSmallIcon(R.drawable.egg)
                 .setContentTitle("Nowe Powiadomienie")
                 .setContentText("To jest przykładowe powiedomienie")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
@@ -53,5 +64,38 @@ public class MainActivity extends AppCompatActivity {
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         notificationManager.notify(1, builder.build());
+    }
+    private void sendNotificationLong(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
+                    !=PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 1);
+                return;
+            }
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.egg)
+                .setContentTitle("Nowe Powiadomienie Long ")
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquam auctor mi, eget volutpat sem tincidunt ac. Cras nec metus eu lacus feugiat lobortis nec ut nulla. Nam sed gravida tellus. Donec tempor ultricies nunc eget fringilla. In rutrum purus erat, nec tempus magna sagittis et. Vivamus eu viverra sem."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, builder.build());
+    }
+
+    private NotificationCompat.Builder sendNotificationCustom(String title, int id, int icon, NotificationCompat.Bi style, PendingIntent intent){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(icon)
+                .setContentTitle(title)
+                .setStyle(style)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        return builder;
     }
 }
