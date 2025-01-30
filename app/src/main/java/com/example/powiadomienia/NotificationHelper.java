d2040db412843fe480350057aacac4c173ed9b89
KalesoneQ1451 <kacper.jatczak2006@gmail.com>
2025-01-30 13:28:38 +0100

    lekcja2

--- app/src/main/java/com/example/powiadomienia/NotificationHelper.java
+++ app/src/main/java/com/example/powiadomienia/NotificationHelper.java
@@ -1,6 +1,31 @@
 package com.example.powiadomienia;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.app.Notification;
+import android.os.Build;
+
+
 
 import java.nio.channels.Channel;
 
@@ -9,7 +34,44 @@ public class NotificationHelper {
     private static final String CHANNEL_NAME = "kanał Powiadomień";
     private static final int NOTIFIACTION_ID = 1;
 
-    private static  void sendNotification(Context context){
+    private static  void sendNotification(AppCompatActivity activity, Context context, String title, String message, int styleType){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (context.checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(activity, new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 100);
+                return;
+            }
+        }
+        NotificationManager notificationManager = (NotificationManager) activity.getSystemService(Context.NOTIFICATION_SERVICE);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(activity, CHANNEL_ID)
+                .setSmallIcon(R.drawable.egg)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true);
+
+        switch (styleType) {
+            case 1:
+                builder.setStyle(new NotificationCompat.BigTextStyle().bigText(message));
+                break;
+            case 2:
+                Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.egg);
+
+                builder.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap).setBigContentTitle(title));
+                break;
+            case 3:
+                NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+                inboxStyle.addLine(message);
+                inboxStyle.addLine("Dodatkowa linia tekstu");
+                builder.setStyle(inboxStyle);
+                break;
+        }
     }
+
 }
